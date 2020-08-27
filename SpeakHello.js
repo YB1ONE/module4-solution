83f1eb91e7ac8f17ae9154918277ff915eace3b7
BAKACHE Youness <68975533+YB1ONE@users.noreply.github.com>
2020-08-27 14:46:03 +0100

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,14 @@
+
+(function(window){
+
+var HelloSpeaker = {}; 
+
+var speakWord = "Hello";
+
+
+HelloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+window.HelloSpeaker = HelloSpeaker
+})(window);
+
